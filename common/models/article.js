cdf349b1841c7596e57cc18ab6c1416e0b4a46d3
Jérémie Drouet <jeremie.drouet@gmail.com>
2016-03-15 08:16:09 +0100

    Add remote method to Article model

--- common/models/article.js
+++ common/models/article.js
@@ -1,3 +1,28 @@
 module.exports = function(Article) {
 
+  Article.sayHello = function(name, callback) {
+    return callback(null, 'Hello ' + name);
+  }
+
+  Article.remoteMethod(
+    'sayHello',
+    {
+      accepts: {
+        arg: 'name',
+        type: 'string',
+        required: true
+      },
+      returns: {
+        arg: 'message',
+        type: 'string'
+      },
+      http: {
+        path: '/say-hello',
+        verb: 'GET',
+        status: 200,
+        errorStatus: 418
+      }
+    }
+  );
+
 };
